7c52076050fb15431147b78432fb289691dd9d91
GeoffMeier <114256168+GeoffMeier@users.noreply.github.com>
2023-08-23 12:38:05 -0500

    Delete JS directory

--- JS/web demy js/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-let name = "Geoffrey";
-
-function hello(personsName) {
-  console.log("hello" + personsName);
-}
-
-hello(name);
-
-console.log(2 + 2);
--- JS/web demy js/functions/app.js
+++
@@ -1,6 +0,0 @@
-// let die1 = Math.floor(Math.random() * 6) + 1;
-// let die2 = Math.floor(Math.random() * 6) + 1;
-// let die3 = Math.floor(Math.random() * 6) + 1;
-// let die4 = Math.floor(Math.random() * 6) + 1;
-// let die5 = Math.floor(Math.random() * 6) + 1;
-// let die6 = Math.floor(Math.random() * 6) + 1;
